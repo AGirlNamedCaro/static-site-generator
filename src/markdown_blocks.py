ac301580a58b72fe5ab04fbbe6d5e5d3ec75be1e
Carolina Contreras Issa <AGirlNamedCaro@gmail.com>
2024-05-04 20:23:37 -0600

    added markdown blocks & tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,10 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered list"
+block_type_ordered_list = "ordered list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -8,3 +15,31 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    if block.startswith("> "):
+        return block_type_quote
+    if block.startswith("* ") or block.startswith("- "):
+        return block_type_unordered_list
+    if block.startswith("1. "):
+        for line in lines:
+            i = 1
+            if line.startswith(f"{i}. "):
+                return block_type_ordered_list
+    else:
+        return block_type_paragraph
+    
+    
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,14 @@
 import unittest
 from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    block_type_code,
+    block_type_ordered_list,
+    block_type_unordered_list,
+    block_type_quote,
+    block_type_heading,
+    block_type_paragraph,
+    block_to_block_type
+)
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,3 +54,18 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+    def test_block_to_block_type(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "###### heading numero 6"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```code goes here ```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> Wisdom  \n> another piece of wisdom"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = '* unordered list item 1\n * item 2 '
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+        block = "1. don't pick up the phone, you know he's only calling because he's drunk and alone\n 2. Don't let him in, you'll have to kick him out again"
+        self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+
+    
